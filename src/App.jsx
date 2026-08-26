e95e4c50cd70f69882699ec5503e2cea6016e28c
Life Rouby <liferouby@gmail.com>
2026-08-26 16:33:47 -0400

    Add projects gallery with detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
@@ -13,6 +14,7 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="resume" element={<Resume />} />
         <Route path="projects" element={<Projects />} />
+        <Route path="projects/:slug" element={<ProjectDetail />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Route>
